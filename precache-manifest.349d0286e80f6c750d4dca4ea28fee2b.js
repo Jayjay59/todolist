559d1034a7bacc4dafd665147d2fb0a3d3b96e8b
Jayjay59 <60185476+Jayjay59@users.noreply.github.com>
2020-09-08 09:54:01 +0200

    Updates

---
+++ precache-manifest.349d0286e80f6c750d4dca4ea28fee2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556383e1051da221ab5d6a7e83c71784",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "c3b5b732b75522a8319d",
+    "url": "/todolist/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c3b5b732b75522a8319d",
+    "url": "/todolist/static/js/2.bc06dcd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.bc06dcd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "affe3c403e9f3945e364",
+    "url": "/todolist/static/js/main.b494130e.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
